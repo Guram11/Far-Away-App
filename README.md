b72d5fd4633d721a317f6392bcd8882953bfc8e0
Guram11 <guramchubinidze11@gmail.com>
2023-06-17 14:42:38 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Far Away App
+
+Add, sort, pack and keep track of all items you need for your trip!
---
+++ src/components/Form.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="add-form">
+      <h3>What do you need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        type="text"
+        placeholder="Item..."
+        value={description}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+export default Form;
